aacae231468d6f5d7e633d79c3f36a0563870de2
AlexandreSlama75 <71281036+AlexandreSlama75@users.noreply.github.com>
2020-10-23 17:28:15 +0100

    Add files via upload

---
+++ Virtual Global College/Virtual Global College/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtual_Global_College
+{
+    public class Student : User
+    {
+        private string branch;
+        private string[,] timetable;
+
+        public Student(string name, string surname, string id, string phoneNumber, string sexe, string mail, string password, string branch, string[,] timetable)
+            : base(name, surname, id, phoneNumber, sexe, mail, password)
+        {
+            Branch = branch;
+            Timetable = timetable;
+        }
+
+        public string Branch
+        {
+            get { return branch; }
+            set
+            {
+                if (value != "ESILV" || value != "EMLV" || value != "IIM")
+                    throw new Exception("This branch doesn't exist");
+                else
+                    branch = value;
+            }
+        }
+        public string[,] Timetable
+        {
+            get { return timetable; }
+            set
+            {
+                for (int i = 0; i < timetable.GetLength(0); i++)
+                    for (int j = 0; j < timetable.GetLength(1); j++)
+                    {
+                        if (timetable[i, j] == null)
+                            throw new Exception("The time slot [" + i + "," + j + "] isn't valid");
+                    }
+                timetable = value;
+            }
+        }
+
+        public override string ToString() => $"{base.ToString()}\n\nType : Student\n Branch : {branch}";
+
+        public void CourseRegistration()
+        {
+
+        }
+    }
+}
---
+++ Virtual Global College/Virtual Global College/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtual_Global_College
+{
+    public abstract class User
+    {
+        public string Name { get; }
+        public string Surname { get; }
+        public string Id { get; }
+        public string PhoneNumber { get; }
+        public string Sexe { get; }
+        public string Mail { get; }
+        public string Password { get; }
+
+        public User (string name, string surname, string id, string phoneNumber, string sexe, string mail, string password)
+        {
+            Name = name;
+            Surname = surname;
+            Id = id;
+            PhoneNumber = phoneNumber;
+            Sexe = sexe;
+            Mail = mail;
+            Password = password;
+        }
+
+        public override string ToString()
+        {
+            return $"Profile :/n/n{Name} {Surname}/nId : {Id}/nPhone number : {PhoneNumber}/nSexe : {Sexe}/nMail : {Mail}/nPassword : {Password}";
+        }
+
+        public bool VerifyLogin(string passwordToVerify)
+        {
+            bool passwordVerified = false;
+            
+            if (passwordToVerify == Password)
+                passwordVerified = true;
+            
+            return passwordVerified;
+        }
+    }
+}
