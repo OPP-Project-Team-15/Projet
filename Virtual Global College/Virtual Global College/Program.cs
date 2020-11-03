ae06cc01b2df36e6070f7a5ec823297437cb495b
AlexandreSlama75 <71281036+AlexandreSlama75@users.noreply.github.com>
2020-11-03 20:31:47 +0000

    Add files via upload

--- Virtual Global College/Virtual Global College/Program.cs
+++ Virtual Global College/Virtual Global College/Program.cs
@@ -1,15 +1,78 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Virtual_Global_College
 {
-    class Program
+    class Program 
     {
+
+        static void Student_Courses_Attribution(Student student, SortedList<string, List<Student>> Student_Courses)
+        {
+            int index;
+            foreach (string element in student.CoursesPicked)
+            {
+                index = Student_Courses.IndexOfKey(element);
+                Student_Courses.Values.ElementAt(index).Add(student);
+            }
+        }
+
+
         static void Main(string[] args)
         {
+
+            string[,] timetable = { { "ok" } };
+            Student student1 = new Student("Jake", "Smith", "1234", "0601020304", "male", "jakesmith@gmail.com", "jsmith", "ESILV", timetable);
+            Student student2 = new Student("Jake", "Smith", "1234", "0601020304", "male", "jakesmith@gmail.com", "jsmith", "ESILV", timetable);
+
+            List<string> courses_name = new List<string>();
+            courses_name.Add("Fluids Mechanics");
+            courses_name.Add("Statistical Inference");
+            courses_name.Add("OOP");
+            courses_name.Add("Numerical Analysis");
+            courses_name.Add("Data Structure");
+
+            
+
+            
+            SortedList<string, List<Student>> Student_Courses = new SortedList<string, List<Student>>();
+
+            List<Student> Student_FluidsMechanics = new List<Student>();
+            List<Student> Student_StatisticalInference = new List<Student>();
+            List<Student> Student_OOP = new List<Student>();
+            List<Student> Student_NumericalAnalysis = new List<Student>();
+            List<Student> Student_DataStructure = new List<Student>();
+
+            Student_Courses.Add("Fluids Mechanics", Student_FluidsMechanics);
+            Student_Courses.Add("Statistical Inference", Student_StatisticalInference);
+            Student_Courses.Add("OOP", Student_OOP);
+            Student_Courses.Add("Numerical Analysis", Student_NumericalAnalysis);
+            Student_Courses.Add("Data Structure", Student_DataStructure);
+
+
+            student1.CourseRegistration(courses_name);
+            Student_Courses_Attribution(student1, Student_Courses);
+            Console.Write($"\n\n Students in OOP : ");
+            foreach (Student student in Student_OOP)
+                Console.Write($"{student.Name}");
+            Console.Write($"\n Students in Fluids Mechanics : ");
+            foreach (Student student in Student_FluidsMechanics)
+                Console.Write($"{student.Name}");
+            Console.Write($"\n Students in Numerical Analysis : ");
+            foreach (Student student in Student_NumericalAnalysis)
+                Console.Write($"{student.Name}");
+
+
+
+
+
+            Console.ReadKey();
+            
         }
     }
 }
--- Virtual Global College/Virtual Global College/Student.cs
+++ Virtual Global College/Virtual Global College/Student.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Virtual_Global_College
 {
+    interface ISchool
+    {
+        string SchoolName {get;}
+        string SchoolInfo();
+    }
+
+    
+
     public class Student : User
     {
         private string branch;
         private string[,] timetable;
+        private string schoolName;
+
+        
+
 
         public Student(string name, string surname, string id, string phoneNumber, string sexe, string mail, string password, string branch, string[,] timetable)
             : base(name, surname, id, phoneNumber, sexe, mail, password)
@@ -17,13 +31,17 @@ namespace Virtual_Global_College
             Branch = branch;
             Timetable = timetable;
         }
-
+        
+        public string SchoolName
+        {
+            get {return schoolName;}
+        }
         public string Branch
         {
             get { return branch; }
             set
             {
-                if (value != "ESILV" || value != "EMLV" || value != "IIM")
+                if (value != "ESILV" && value != "EMLV" && value != "IIM")
                     throw new Exception("This branch doesn't exist");
                 else
                     branch = value;
@@ -34,21 +52,52 @@ namespace Virtual_Global_College
             get { return timetable; }
             set
             {
-                for (int i = 0; i < timetable.GetLength(0); i++)
-                    for (int j = 0; j < timetable.GetLength(1); j++)
+                for (int i = 0; i < value.GetLength(0); i++)
+                    for (int j = 0; j < value.GetLength(1); j++)
                     {
-                        if (timetable[i, j] == null)
+                        if (value[i, j] == null)
                             throw new Exception("The time slot [" + i + "," + j + "] isn't valid");
                     }
                 timetable = value;
             }
         }
+        public string[] CoursesPicked { get; set; }
 
         public override string ToString() => $"{base.ToString()}\n\nType : Student\n Branch : {branch}";
 
-        public void CourseRegistration()
+        public void CourseRegistration(List<string> courses)
         {
+            string[] picks = new string[2];
 
+            Console.WriteLine("Which courses would you want to pick ?\n");
+            int compt = 1;
+            foreach (string element in courses)
+            {
+                Console.WriteLine($"{compt} - {element}");
+                compt++;
+            }
+            Console.WriteLine();
+
+            string answer;
+            answer = Console.ReadLine();
+
+
+            for (int i = 0; i < 2; i++)
+            {
+                while (courses.Contains(answer) != true)
+                {
+                    Console.WriteLine("This course doesn't exist. Please select another one\n");
+                    answer = Console.ReadLine();
+                }
+                Console.WriteLine($"The course {answer} has been taken\n");
+                picks[i] = answer;
+
+                if (i < 2)
+                    answer = Console.ReadLine();
+            }
+
+            Console.Write($"Thank you for your participation");
+            this.CoursesPicked = picks;
         }
     }
 }
