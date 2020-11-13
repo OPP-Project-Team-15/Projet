a3a4a899c72c09e37b76be676ea6614677cee33d
Alexandre <alexandrez75@gmail.com>
2020-11-13 19:15:11 +0000

    Last

--- Virtual Global College/Virtual Global College/Admin.cs
+++ Virtual Global College/Virtual Global College/Admin.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 namespace Virtual_Global_College
 {
     public class Admin : User
-    {
+    {/*
         private string [,] timetableOfClass;
 
         public Admin(string name, string surname, string id, string phoneNumber, string sexe, string mail, string password, string[,] timetableOfClass) 
         : base (name, surname, id, phoneNumber, sexe, mail, password)
         {
             TimetableOfClass = timetableOfClass;
-        }
+        }*/
     }
     
     
