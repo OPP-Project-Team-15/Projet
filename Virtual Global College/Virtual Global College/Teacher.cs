965f1a4ac5810681ff4d8f754a734e7832727bbe
Alexandre <alexandrez75@gmail.com>
2020-11-14 21:43:11 +0000

    Add_Assignment method modifications

--- Virtual Global College/Virtual Global College/Teacher.cs
+++ Virtual Global College/Virtual Global College/Teacher.cs
@@ -20,27 +20,36 @@ namespace Virtual_Global_College
 
         public void Add_Assignment()
         {
-            Console.WriteLine("Specify the month number of the assignment");
+            Console.WriteLine("Specify the month number of the assignment :");
             int month = Convert.ToInt32(Console.ReadLine());
             while (month < 1 && month > 12)
             {
                 Console.WriteLine("Please write a mounth number between 1 and 12");
                 month = Console.ReadLine();
             }
-            Console.WriteLine("Specify the day number of the assignment");
+            
+            Console.WriteLine("\nSpecify the day number of the assignment :");
             int day = Convert.ToInt32(Console.ReadLine());
             while (day < 1 && day > 31)
             {
                 Console.WriteLine("Please write a mounth number between 1 and 12");
                 day = Console.ReadLine();
             }
-            Console.WriteLine("Specify the year of the assignment");
+            
+            Console.WriteLine("\nSpecify the year of the assignment :");
             int year = Convert.ToInt32(Console.ReadLine());
             while (year < 2020)
             {
-                Console.WriteLine("Please write a mounth number between 1 and 12");
+                Console.WriteLine("Please write a year of 4 numbers, and superior than 2019");
                 year = Console.ReadLine();
             }
+
+            DateTime assignmentDate = new DateTime(year, month, day);
+
+            Console.WriteLine("\nSpecify the assignment :")
+            string assignmentContent = Console.ReadLine();
+
+            string assignment = $"{assignmentDate.Month}/{assignmentDate.Day}/{assignmentDate.Year} {assignmentContent}";
         }
     }
 }
