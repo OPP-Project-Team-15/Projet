eef371f21efb18759179af3859ee35c25d07a478
remi <remi.warter@edu.devinci.fr>
2020-11-15 03:01:12 +0000

    Modif Admin

--- Virtual Global College/Virtual Global College/Admin.cs
+++ Virtual Global College/Virtual Global College/Admin.cs
@@ -6,16 +6,14 @@ using System.Threading.Tasks;
 
 namespace Virtual_Global_College
 {
-    public class Admin
-    {/*
-        private string [,] timetableOfClass;
+    public class Admin : User
+    {
+        public string[,] TimetableOfClass { get; set; }
 
         public Admin(string name, string surname, string id, string phoneNumber, string sexe, string mail, string password, string[,] timetableOfClass) 
         : base (name, surname, id, phoneNumber, sexe, mail, password)
         {
             TimetableOfClass = timetableOfClass;
-        }*/
+        }
     }
-    
-    
 }
