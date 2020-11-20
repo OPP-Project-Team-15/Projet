830be92f50523bea86c6c7c5ad67885e7865b2b4
Alexandre <alexandrez75@gmail.com>
2020-11-20 22:13:23 +0000

    Link to SQL

--- Virtual Global College/Virtual Global College/Program.cs
+++ Virtual Global College/Virtual Global College/Program.cs
@@ -28,23 +28,54 @@ namespace Virtual_Global_College
         
         public static void Insert(SqlConnection conn, SqlCommand cmd)
         {
-            Console.WriteLine("Veuillez rentrer vos informations \n 1 - Nom ");
-            string nom = Console.ReadLine();
-            Console.WriteLine("\n 2 - Prenom ");
-            string prenom = Console.ReadLine();
-            Console.WriteLine("\n 3 - Date de naissance (YYYY-MM-DD) ");
-            string dateNaissance = Console.ReadLine();
-            Console.WriteLine("\n 4 - Ville ");
-            string ville = Console.ReadLine();
-            Console.WriteLine("\n 5 - idSubject ");
-            int idSubject = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your informations \n 1 - Last name :");
+            string lastName = Console.ReadLine();
+            
+            Console.WriteLine("\n 2 - First name :");
+            string firstName = Console.ReadLine();
+            
+            Console.WriteLine("\n 3 - Date of birth (MM-DD-YYYY) :");
+            string stringBirthDate = Console.ReadLine();
+            char[] split = { '-' };
+            string[] birthDateSplited = stringBirthDate.Split(split);
+            DateTime birthDate = new DateTime(Convert.ToInt32(birthDateSplited[2]), Convert.ToInt32(birthDateSplited[0]), Convert.ToInt32(birthDateSplited[1]));
+            
+            Console.WriteLine("\n 4 - Sexe (write 'Male' or 'Female') :");
+            string sexe = Console.ReadLine();
+            while (sexe != "Male" && sexe != "Female")
+            {
+                Console.WriteLine("Please write 'Male' or 'Female'");
+                sexe = sexe = Console.ReadLine();
+            }
+            
+            Console.WriteLine("\n 5 - Phone number :");
+            string phoneNumber = Console.ReadLine();
+            
+            Console.WriteLine("\n 6 - Mail :");
+            string mail = Console.ReadLine();
+            while (mail.Contains('@') == false)
+            {
+                Console.WriteLine("This adress doesn't countains '@'. Please write a correct mail adress");
+                mail = Console.ReadLine();
+            }
+            
+            Console.WriteLine("\n 7 - Your new password :");
+            string password = Console.ReadLine();
+            while (password == password.ToLower() || !password.Any(char.IsLetter) || !password.Any(char.IsDigit))
+            {
+                Console.WriteLine("The password must contain : a capital letter, a lower letter and a number");
+                password = Console.ReadLine();
+            }
+            
+            Console.WriteLine("\n 8 - Branch (write 'ESILV', 'EMLV' or 'IIM') :");
+            string branch = Console.ReadLine();
+
 
             try
             {
-                string sql = ($"INSERT INTO Students (FirstName, LastName, DateNaissance, Ville, IdSubject) VALUES ('{prenom}', '{nom}', '{dateNaissance}', '{ville}', '{idSubject}')");
-                //Console.WriteLine("Connecting to MySQL...");
+                string sql = $"INSERT INTO Students (LastName, FirstName, BirthDate, Sexe, PhoneNumber, Mail, Password, Branch) VALUES ('{lastName}', '{firstName}', '{birthDate}', '{sexe}', '{phoneNumber}', '{mail}', '{password}', '{branch}')";
                 conn.Open();
-                cmd = new MySqlCommand(sql, conn);
+                cmd = new SqlCommand(sql, conn);
                 cmd.ExecuteNonQuery();
             }
             catch (Exception ex)
@@ -59,25 +90,27 @@ namespace Virtual_Global_College
 
         public static void Main(string[] args)
         {
+            string mot = "OK1";
+            Console.WriteLine(mot == mot.ToLower());
+            Console.ReadKey();
+            Console.WriteLine("\n 7 - Your new password :");
+            string password = Console.ReadLine();
+            while (password == password.ToLower() || password.All(char.IsLetter) || password.All(char.IsDigit))
+            {
+                Console.WriteLine("The password must contain : a capital letter, a lower letter and a number");
+                password = Console.ReadLine();
+            }
+            Console.ReadKey();
+
             // LINK TO SQL
             string connectionString = @"Data Source=DESKTOP-GHLL41C\SQLEXPRESS;Initial Catalog=Virtual Global College;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            SqlConnection cnn = new SqlConnection(connectionString);
-            cnn.Open();
-            
-            string sql = "SELECT Password FROM Students";
-            SqlCommand command = new SqlCommand(sql, cnn);
-            SqlDataReader dataReader = command.ExecuteReader();
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = null;
+            //SqlDataReader dataReader = null;
 
-            while (dataReader.Read())
-            {
-                Console.WriteLine(dataReader[0]);
-            }
+            Insert(conn, cmd);
 
-            dataReader.Close();
-            command.Dispose();
-            cnn.Close();
-
-            //Console.ReadKey();
+            Console.ReadKey();
             // END OF LINK TO SQL
 
 
