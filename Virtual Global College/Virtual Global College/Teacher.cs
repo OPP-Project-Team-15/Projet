e1eaad7626f4bc54e9e11c3031f49c91e58be170
Thomas d'anglejan <thomasdanglejan@gmail.com>
2020-11-21 16:55:04 +0000

    Update code

--- Virtual Global College/Virtual Global College/Teacher.cs
+++ Virtual Global College/Virtual Global College/Teacher.cs
@@ -8,18 +8,27 @@ namespace Virtual_Global_College
 {
     public class Teacher : User
     {
-        public SortedList<string, List<Student>> Subjects { get; set; }
+        public SortedList<string, List<Student>> SubjectStudent { get; set; }
+        public SortedList<string, List<string[,]>> Grade { get; set; }
 
         public Teacher (string name, string surname, string id, string phoneNumber, string sexe, string mail, string password, SortedList<string, List<Student>> subject)
             : base(name, surname, id, phoneNumber, sexe, mail, password)
         {
-            Subjects = subject;
+            SubjectStudent = subject;
         }
 
-        public override string ToString() => $"{base.ToString()}\n\nSubject : {Subjects}";
+        public override string ToString() => $"{base.ToString()}\n\nSubject : {SubjectStudent}";
 
         public string CreateAssignment()
         {
+            Console.WriteLine("Specify the subject of the assignment :");
+            string subj = Console.ReadLine();
+            while (SubjectStudent.ContainsKey(subj) != true)
+            {
+                Console.WriteLine("Please write a mounth number between 1 and 12");
+                subj = Console.ReadLine();
+            }
+
             Console.WriteLine("Specify the month number of the assignment :");
             int month = Convert.ToInt32(Console.ReadLine());
             while (month < 1 && month > 12)
@@ -49,7 +58,7 @@ namespace Virtual_Global_College
             Console.WriteLine("\nSpecify the assignment :");
             string assignmentContent = Console.ReadLine();
 
-            string assignment = $"{assignmentDate.Month}/{assignmentDate.Day}/{assignmentDate.Year} {assignmentContent}";
+            string assignment = $"{subj} {assignmentDate.Month}/{assignmentDate.Day}/{assignmentDate.Year} {assignmentContent}";
 
             return assignment;
         }
@@ -66,7 +75,7 @@ namespace Virtual_Global_College
             {
                 Console.WriteLine("\nIn which subject do you want to put grade ?");
                 choice = Console.ReadLine();
-                subjectExist = Subjects.Keys.Contains(choice);
+                subjectExist = SubjectStudent.Keys.Contains(choice);
             }
             Console.WriteLine("\nGive a name for the assignment :");
             string nameAssignment = Console.ReadLine();
@@ -89,17 +98,19 @@ namespace Virtual_Global_College
                 Console.WriteLine("From which student do you want to see the coordinate. Write a surname of a student who exist in your class.");
                 string surname = Console.ReadLine();
                 int index = 0;
-                foreach (KeyValuePair<string,List<Student>> subj in Subjects)
+                foreach (KeyValuePair<string,List<Student>> subj in SubjectStudent)
                 {
                     index++;
                     if (nameExist == false)
                     {
-                        foreach (Student stud in Subjects.Values[index])
+                        foreach (Student stud in SubjectStudent.Values[index])
                         {
                             nameExist = stud.Surname.Contains(surname);
                             if (nameExist == true)
                             {
                                 student = stud;
+                                student.ToString();
+                                Console.WriteLine();
                             }
                         }
                     }
@@ -113,6 +124,63 @@ namespace Virtual_Global_College
                     while (answer != "YES" || answer != "NOT")
                     {
                         Console.WriteLine("Do you want to see the coordinates of an other student ? Answer YES or NOT");
+                        answer = Console.ReadLine();
+                    }
+                    if (answer == "YES")
+                    {
+                        nameExist = false;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Allow the teacher to put a new grade 
+        /// </summary>
+        public void CreateGrade()
+        {
+
+        }
+
+
+        /// <summary>
+        /// Allow the teacher to see all the attendance of a specific student
+        /// </summary>
+        public void Attendance()
+        {
+            bool nameExist = false;
+            Student student = null;
+            while (nameExist == false)
+            {
+                Console.WriteLine("From which student do you want to see the attendance. Write a surname of a student who exist in your class.");
+                string surname = Console.ReadLine();
+                int index = 0;
+                foreach (KeyValuePair<string, List<Student>> subj in SubjectStudent)
+                {
+                    index++;
+                    if (nameExist == false)
+                    {
+                        foreach (Student stud in SubjectStudent.Values[index])
+                        {
+                            nameExist = stud.Surname.Contains(surname);
+                            if (nameExist == true)
+                            {
+                                student = stud;
+                                student.ToStringShowTheAttendance();
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+                }
+
+                if (nameExist == true)
+                {
+                    student.ToString();
+                    Console.WriteLine("Do you want to see the attendance of an other student ? Answer YES or NOT");
+                    string answer = Console.ReadLine();
+                    while (answer != "YES" || answer != "NOT")
+                    {
+                        Console.WriteLine("Do you want to see the attendance of an other student ? Answer YES or NOT");
                         answer = Console.ReadLine();
                     }
                     if (answer == "YES")
