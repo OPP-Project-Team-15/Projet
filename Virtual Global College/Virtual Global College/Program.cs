333aa7e0169fbd3f41476a297dc013cf6f0c237f
Thomas d'anglejan <thomasdanglejan@gmail.com>
2020-12-05 17:27:25 +0000

    Ok

--- Virtual Global College/Virtual Global College/Program.cs
+++ Virtual Global College/Virtual Global College/Program.cs
@@ -407,39 +407,19 @@ namespace Virtual_Global_College
             MySqlCommand cmd = null;
             MySqlDataReader rdr = null;
 
-            bool pass; bool exist;
+            bool pass = false; bool exist;
             string id = " ";
-            
+            int j = 0;
             string sql;
+            bool forgetPassword = false;
 
-            Console.Write("ID : ");
-            id = Console.ReadLine();
-            sql = "SELECT Id From Students";
-            cmd = new MySqlCommand(sql, conn);
-            exist = Exist(id, conn, cmd, rdr);
-
-            if (exist == false)
+            while (forgetPassword == false)
             {
-                sql = "SELECT Id From Teachers";
-                cmd = new MySqlCommand(sql, conn);
-                exist = Exist(id, conn, cmd, rdr);
-            }
-
-            if (exist == false)
-            {
-                sql = "SELECT Id From Admins";
-                cmd = new MySqlCommand(sql, conn);
-                exist = Exist(id, conn, cmd, rdr);
-            }
-
-            while (exist == false)
-            {
-                Console.Write("This ID doesn't exist. Please enter an existing ID : ");
+                Console.Write("ID : ");
                 id = Console.ReadLine();
-
-                sql = "SELECT Id FROM Students";
+                sql = "SELECT Id From Students";
                 cmd = new MySqlCommand(sql, conn);
-                exist = Program.Exist(id, conn, cmd, rdr);
+                exist = Exist(id, conn, cmd, rdr);
 
                 if (exist == false)
                 {
@@ -454,51 +434,97 @@ namespace Virtual_Global_College
                     cmd = new MySqlCommand(sql, conn);
                     exist = Exist(id, conn, cmd, rdr);
                 }
-            }
 
-            Console.Write("Password : ");
-            var password = ReadPassword();
-            sql = $"SELECT Password FROM Students WHERE Id = '{id}'";
-            cmd = new MySqlCommand(sql, conn);
-            pass = Exist(password, conn, cmd, rdr);
+                while (exist == false)
+                {
+                    Console.Write("This ID doesn't exist. Please enter an existing ID : ");
+                    id = Console.ReadLine();
 
-            if (pass == false)
-            {
-                sql = $"SELECT Password From Teachers WHERE Id = '{id}' ";
-                cmd = new MySqlCommand(sql, conn);
-                pass = Exist(password, conn, cmd, rdr);
-            }
+                    sql = "SELECT Id FROM Students";
+                    cmd = new MySqlCommand(sql, conn);
+                    exist = Program.Exist(id, conn, cmd, rdr);
 
-            if (pass == false)
-            {
-                sql = $"SELECT Password From Admins WHERE Id = '{id}' ";
-                cmd = new MySqlCommand(sql, conn);
-                pass = Exist(password, conn, cmd, rdr);
-            }
+                    if (exist == false)
+                    {
+                        sql = "SELECT Id From Teachers";
+                        cmd = new MySqlCommand(sql, conn);
+                        exist = Exist(id, conn, cmd, rdr);
+                    }
 
-            while (pass == false)
-            {
-                Console.Write("Wrong password. Please type again: ");
-                password = ReadPassword();
+                    if (exist == false)
+                    {
+                        sql = "SELECT Id From Admins";
+                        cmd = new MySqlCommand(sql, conn);
+                        exist = Exist(id, conn, cmd, rdr);
+                    }
+                }
 
+                Console.Write("Password : ");
+                var password = ReadPassword();
                 sql = $"SELECT Password FROM Students WHERE Id = '{id}'";
                 cmd = new MySqlCommand(sql, conn);
-                pass = Program.Exist(password, conn, cmd, rdr);
+                pass = Exist(password, conn, cmd, rdr);
 
                 if (pass == false)
                 {
-                    sql = $"SELECT Password From Teachers WHERE Id = '{id}'";
+                    sql = $"SELECT Password From Teachers WHERE Id = '{id}' ";
                     cmd = new MySqlCommand(sql, conn);
                     pass = Exist(password, conn, cmd, rdr);
                 }
 
                 if (pass == false)
                 {
-                    sql = $"SELECT Password From Admins WHERE Id = '{id}'";
+                    sql = $"SELECT Password From Admins WHERE Id = '{id}' ";
                     cmd = new MySqlCommand(sql, conn);
                     pass = Exist(password, conn, cmd, rdr);
+                }
+
+                forgetPassword = true;
+
+                while (pass == false)
+                {
+                    j += 1;
+                    Console.Write("Wrong password. Please type again: ");
+                    password = ReadPassword();
+
+                    sql = $"SELECT Password FROM Students WHERE Id = '{id}'";
+                    cmd = new MySqlCommand(sql, conn);
+                    pass = Program.Exist(password, conn, cmd, rdr);
+
+                    if (pass == false)
+                    {
+                        sql = $"SELECT Password From Teachers WHERE Id = '{id}'";
+                        cmd = new MySqlCommand(sql, conn);
+                        pass = Exist(password, conn, cmd, rdr);
+                    }
+
+                    if (pass == false)
+                    {
+                        sql = $"SELECT Password From Admins WHERE Id = '{id}'";
+                        cmd = new MySqlCommand(sql, conn);
+                        pass = Exist(password, conn, cmd, rdr);
+                    }
+
+                    forgetPassword = true;
+
+                    if (j > 2)
+                    {
+                        Console.WriteLine("You have to change your password ? Write a new one");
+                        string passwordForget = Console.ReadLine();
+                        sql = $"UPDATE Students SET Password ='{passwordForget}' WHERE Id = @id";
+                        cmd = new MySqlCommand(sql, conn);
+                        cmd.Prepare();
+                        cmd.Parameters.AddWithValue("@id", id);
+                        Program.Insert(conn, cmd, rdr);
+                        Console.WriteLine("\nYour password has been changed");
+                        forgetPassword = false;
+                        pass = true;
+                        Console.Clear();
+                    }
                 }
             }
+            
+            
 
             Console.WriteLine("Success");
             ConsoleSpiner spin = new ConsoleSpiner();
